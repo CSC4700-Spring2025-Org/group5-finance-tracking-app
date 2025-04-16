9b20baf78f4e7121b8d0c75f32188b1f058db1ac
rmassaroni <robjmass@gmail.com>
2025-04-16 17:51:52 -0400

    styling fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,20 @@ import RecordList from './accounting/RecordList';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<FinancialDashboard />} />
-        <Route path="/accounting/balance-sheet" element={<BalanceSheet />} />
-        <Route path="/reports/income-statement" element={<div className="p-8 text-center">Income Statement (Coming Soon)</div>} />
-        <Route path="/reports/cash-flows" element={<div className="p-8 text-center">Statement of Cash Flows (Coming Soon)</div>} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<FinancialDashboard />} />
+                <Route path="/accounting/balance-sheet" element={
+                    <div className="App-header">
+                        <BalanceSheet />
+                    </div>
+                } />
+                <Route path="/reports/income-statement" element={<div className="p-8 text-center">Income Statement (Coming Soon)</div>} />
+                <Route path="/reports/cash-flows" element={<div className="p-8 text-center">Statement of Cash Flows (Coming Soon)</div>} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
